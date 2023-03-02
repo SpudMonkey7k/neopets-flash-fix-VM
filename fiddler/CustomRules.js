54066686d6a7b58ad04e317ff9b022fcc15da16e
themrrobert <10122432+themrrobert@users.noreply.github.com>
2023-03-02 09:02:15 -0800

    Fix multiple escape bug

--- fiddler/CustomRules.js
+++ fiddler/CustomRules.js
@@ -334,7 +334,7 @@ class Handlers
 							respBody.Replace('redirect("post")','redirect("reload")');
 						} else if (oSession.HTTPMethodIs('POST')) {
 							// Fix the 'addFields' function in stackpath to actually add the form data (and update the Referer):
-							var replacementStr = "function addFields(formObj){const fTarget = 'FORM_ACTION'; const postData = 'FORM_DATA';const previousPage = 'PREV_PAGE';const fields = postData.split('&');for (const field of fields) {const parts = field.split('=');const newMem = document.createElement('input');newMem.type = 'hidden';newMem.name = parts[0];newMem.value = parts[1];formObj.appendChild(newMem);}window.history.replaceState(null, '', previousPage);formObj.action=fTarget;}";
+							var replacementStr = "function addFields(formObj){const fTarget = 'FORM_ACTION'; const postData = 'FORM_DATA';const previousPage = 'PREV_PAGE';const fields = postData.split('&');for (const field of fields) {const parts = field.split('=');const newMem = document.createElement('input');newMem.type = 'hidden';newMem.name = parts[0];newMem.value = unescape(parts[1]);formObj.appendChild(newMem);}window.history.replaceState(null, '', previousPage);formObj.action=fTarget;}";
 							var data = oSession.GetRequestBodyAsString();
 							var prev = oSession.oRequest.headers['Referer'];
 							replacementStr = replacementStr.replace('FORM_ACTION', oSession.fullUrl);
